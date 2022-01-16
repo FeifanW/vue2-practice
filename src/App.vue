477cd722090e19eea3acf1f901606af83c37574f
FeifanW <qihang791@163.com>
2022-01-16 10:10:18 +0800

    练习animate.css库

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   <div id="app">
     <Test></Test>
     <Test2></Test2>
+    <Test3></Test3>
   </div>
 </template>
 <script>
 import Test from './components/Test.vue'
 import Test2 from './components/Test2.vue'
+import Test3 from './components/Test3.vue'
 export default {
-  components:{Test,Test2}
+  components:{Test,Test2,Test3}
 }
 </script>
---
+++ src/components/Test3.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">显示/隐藏</button>
+    <transition-group 
+        name="animate__animated animate__bounce" 
+        appear
+        enter-active-class="animate__swing"
+        leave-active-class="animate__backOutUp"
+    >
+        <h1 v-show="isShow" key="1" class="come">你好啊</h1>
+        <h1 v-show="isShow" key="2" class="come">加油啊</h1></h1>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+import 'animate.css'
+export default {
+  name: 'Test',
+  data(){
+      return {
+          isShow:true
+      }
+  },
+  methods:{
+
+  },
+}
+</script>
+<style scoped>
+    h1{
+        background-color: orange;
+    }
+
+
+</style>
