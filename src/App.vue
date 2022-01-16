3368a8aaef08dcc65f6a2673c1cb7d5e38910559
FeifanW <qihang791@163.com>
2022-01-16 09:14:44 +0800

    练习vue的动画和过渡

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
     <Test></Test>
+    <Test2></Test2>
   </div>
 </template>
 <script>
 import Test from './components/Test.vue'
+import Test2 from './components/Test2.vue'
 export default {
-  components:{Test}
+  components:{Test,Test2}
 }
 </script>
