e80fd1a5b63f9cd6d21ad60a0e5d2e097a9abae5
FeifanW <qihang791@163.com>
2022-01-18 00:05:29 +0800

    安装vue-cropper插件

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueCropper from 'vue-cropper'
 
 Vue.config.productionTip = false
+Vue.use(VueCropper)
 
 const vm = new Vue({
   router,
