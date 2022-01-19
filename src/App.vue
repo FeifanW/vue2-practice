25a1e6310f380a84549a01385bdeae5c4bba2387
FeifanW <qihang791@163.com>
2022-01-19 23:27:33 +0800

    练习vuex

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
   <div id="container">
-    <category>
-      <img slot="first" src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fnimg.ws.126.net%2F%3Furl%3Dhttp%3A%2F%2Fdingyue.ws.126.net%2F2021%2F1011%2F12ca9832j00r0sbu50019c000hs009zc.jpg%26thumbnail%3D650x2147483647%26quality%3D80%26type%3Djpg&refer=http%3A%2F%2Fnimg.ws.126.net&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1644940351&t=928189114369cfef16b6daddf8eae8f0" alt="">
-    </category>
-    <category>
-      <template v-slot:default="test">
-        <div>
-          {{test.hello}}
-        </div>
-      </template>
-    </category>
-    <category>
-    </category>
+    <div>{{num}}</div>
+    <button @click="add">+</button>
   </div>
 </template>
 <script>
 import Category from './components/Category.vue'
 export default {
-  components:{Category}
+  data(){
+    return{
+      num:1
+    }
+  },
+  components:{Category},
+  mounted() {
+    console.log(this)
+  },
+  methods:{
+    add(){
+      this.$store.dispatch('add',1)
+    }
+  }
 }
 </script>
 <style>
--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,16 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+// 创建vuex，并且直接导出
 export default new Vuex.Store({
   state: {
   },
   mutations: {
   },
   actions: {
+    add(){
+      console.log('vuex里的action被调用了')
+    }
   },
   modules: {
   }
