ebb6740d904b5900f7432b96ad25fa47fa01b0dd
FeifanW <qihang791@163.com>
2022-02-03 11:38:39 +0800

    练习element-ui按需引入

--- src/main.js
+++ src/main.js
@@ -3,11 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import VueCropper from 'vue-cropper'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css';
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css';
+
+// 按需引入
+import { Button } from 'element-ui'
+
+Vue.component(Button.name, Button)
 
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+// Vue.use(ElementUI)
 Vue.use(VueCropper)
 
 const vm = new Vue({
